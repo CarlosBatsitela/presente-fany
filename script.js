a99d6f0f937f9e94a865771e8d317090a02e042c
CarlosBatsitela <carlosebb1412@gmail.com>
2026-08-07 00:55:08 -0300

    quase pronto

--- script.js
+++ script.js
@@ -1,133 +1,321 @@
-console.log("Projeto Iniciado ❤️");
+const reiniciar = document.getElementById("reiniciar");
+const textoFinal = document.getElementById("textoFinal");
 
-const botao = document.getElementById("abrirPresente");
-
-const inicio = document.getElementById("inicio");
-
+const abrirPresente = document.getElementById("abrirPresente");
 const presente = document.getElementById("presente");
 
 const abrirCarta = document.getElementById("abrirCarta");
-
 const carta = document.getElementById("carta");
 
-const textoCarta = document.getElementById("textoCarta");
-
 const fotos = document.getElementById("fotos");
-
+const fotoAtual = document.getElementById("fotoAtual");
 const proximaFoto = document.getElementById("proximaFoto");
 
-const fotoAtual = document.getElementById("fotoAtual");
-
+const contador = document.getElementById("contador");
 const tempo = document.getElementById("tempo");
 
-const contador = document.getElementById("contador");
+const final = document.getElementById("final");
+const coracoes = document.getElementById("coracoes");
 
 const musica = document.getElementById("musica");
 
 
-let numeroFoto = 1;
+abrirPresente.addEventListener("click", () => {
 
-const inicioNamoro = new Date("2023-09-04T19:00:00");
+    document.getElementById("inicio").classList.add("oculto");
 
-
-const listaFotos = [
-
-    "imagem/foto1.jpg",
-    "imagem/foto2.jpg",
-    "imagem/foto3.jpg",
-    "imagem/foto4.jpg"
-
-];
-
-botao.addEventListener("click", () => {
-    inicio.classList.add("oculto");
     presente.classList.remove("oculto");
-    musica.play().catch(() => {
 
-    console.log("Navegador bloqueou o áudio");
-    });
+    musica.play();
+
 });
 
+
 abrirCarta.addEventListener("click", () => {
+
     presente.classList.add("oculto");
+
     carta.classList.remove("oculto");
-textoCarta.innerHTML = `
-Vida <br><br>
-Hoje é um dia muito especial, porque é o dia da pessoa mais incrível e especial do mundo pra mim.<br><br>
-Tentei fazer esse simples presente digital para mostrar um pouco do amor que sinto por você.<br><br>
-Espero que você goste. Eu Te Amo Thiff ❤️
-`;
-setTimeout(() => {
 
-    carta.classList.add("oculto");
 
-    fotos.classList.remove("oculto");
+    document.getElementById("textoCarta").innerHTML = `
+
+    Vida ...
+
+    Hoje é um dia muito especial, porque é o dia da pessoa mais incrível e especial do mundo pra mim.
+
+    Tentei fazer esse simples presente para mostrar um pouco do amor que sinto por você.
+
+    Espero que você goste.
+
+    Eu Te Amo Thiff ❤️
+
+    `;
 
 
     setTimeout(() => {
+
+        carta.classList.add("oculto");
+
+        fotos.classList.remove("oculto");
+
+    },10000);
+
+});
+
+
+let foto = 1;
+
+
+proximaFoto.addEventListener("click", () => {
+
+
+    foto++;
+
+
+    if(foto > 4){
 
         fotos.classList.add("oculto");
 
         contador.classList.remove("oculto");
 
-    }, 15000);
+        iniciarContador();
 
-
-}, 8000);
-
-});
-
-proximaFoto.addEventListener("click", () => {
-
-    numeroFoto++;
-
-    if(numeroFoto > listaFotos.length){
-
-        numeroFoto = 1;
+        return;
 
     }
 
-    fotoAtual.style.animation = "none";
 
-setTimeout(() => {
-
-    fotoAtual.style.animation = "trocarFoto .6s ease";
-
-}, 10);
-
-
-fotoAtual.src = listaFotos[numeroFoto - 1];
+    fotoAtual.src = `imagem/foto${foto}.jpg`;
 
 });
 
-function atualizarContador(){
-
-    const agora = new Date();
-
-    const diferenca = agora - inicioNamoro;
 
 
-    const segundos = Math.floor(diferenca / 1000);
-
-    const dias = Math.floor(segundos / 86400);
-
-    const horas = Math.floor((segundos % 86400) / 3600);
-
-    const minutos = Math.floor((segundos % 3600) / 60);
-
-    const segundosRestantes = segundos % 60;
+function iniciarContador(){
 
 
-    tempo.innerHTML = `
-    ${dias} dias ❤️<br>
-    ${horas} horas ❤️<br>
-    ${minutos} minutos ❤️<br>
-    ${segundosRestantes} segundos
-    `;
+    const inicioNamoro = new Date("2023-09-04T19:00:00");
+
+
+    const contadorAtual = setInterval(()=>{
+
+
+        const agora = new Date();
+
+
+        const diferenca = agora - inicioNamoro;
+
+
+        const dias = Math.floor(
+            diferenca / (1000*60*60*24)
+        );
+
+
+        const horas = Math.floor(
+            (diferenca / (1000*60*60)) % 24
+        );
+
+
+        const minutos = Math.floor(
+            (diferenca / (1000*60)) % 60
+        );
+
+
+        const segundos = Math.floor(
+            (diferenca / 1000) % 60
+        );
+
+
+        tempo.innerHTML = `
+
+        ${dias} dias ❤️ <br>
+
+        ${horas} horas
+
+        ${minutos} minutos
+
+        ${segundos} segundos
+
+        `;
+
+
+    },1000);
+
+
+
+    setTimeout(()=>{
+
+
+    clearInterval(contadorAtual);
+
+
+    contador.style.display = "none";
+
+
+    final.style.display = "flex";
+
+
+    escreverFinal();
+
+
+    iniciarCoracoes();
+
+
+},8000);
+
 
 }
 
 
-setInterval(atualizarContador,1000);
 
-atualizarContador();
+
+function escreverFinal(){
+
+    const titulo = document.querySelector(".mensagemFinal h1");
+
+    const texto = "Eu Te Amo ❤️";
+
+    titulo.innerHTML = "";
+
+    let i = 0;
+
+
+    const escrever = setInterval(()=>{
+
+        titulo.innerHTML += texto[i];
+
+        i++;
+
+
+        if(i >= texto.length){
+
+            clearInterval(escrever);
+
+            escreverMensagem();
+
+        }
+
+    },200);
+
+}
+
+function escreverFinal(){
+
+    const titulo = document.querySelector(".mensagemFinal h1");
+
+    const texto = "Eu Te Amo ❤️";
+
+    titulo.innerHTML = "";
+
+    let i = 0;
+
+
+    const escrever = setInterval(()=>{
+
+        titulo.innerHTML += texto[i];
+
+        i++;
+
+
+        if(i >= texto.length){
+
+            clearInterval(escrever);
+
+            animarMensagem();
+
+        }
+
+    },200);
+
+}
+
+function animarMensagem(){
+
+    const mensagem = document.querySelector(".mensagemFinal p");
+
+    mensagem.style.opacity = "0";
+
+    mensagem.style.transform = "translateY(30px)";
+
+
+    setTimeout(()=>{
+
+        mensagem.style.transition = "1s";
+
+        mensagem.style.opacity = "1";
+
+        mensagem.style.transform = "translateY(0)";
+
+
+    },500);
+
+}
+
+
+function iniciarCoracoes(){
+
+
+    setInterval(()=>{
+
+
+        const coracao = document.createElement("span");
+
+
+        coracao.innerHTML = "❤️";
+
+
+        coracao.classList.add("coracao");
+
+
+        coracao.style.left =
+        Math.random()*100+"vw";
+
+
+        coracao.style.animationDuration =
+        (Math.random()*3+3)+"s";
+
+
+        coracoes.appendChild(coracao);
+
+
+
+        setTimeout(()=>{
+
+
+            coracao.remove();
+
+
+        },6000);
+
+
+
+    },300);
+
+
+}
+
+reiniciar.addEventListener("click",()=>{
+
+    location.reload();
+
+});
+
+setTimeout(()=>{
+
+    clearInterval(contadorAtual);
+
+    contador.style.display = "none";
+
+    final.style.display = "flex";
+
+    escreverFinal();
+
+    setTimeout(()=>{
+
+        iniciarCoracoes();
+
+    },3000);
+
+
+},15000);
